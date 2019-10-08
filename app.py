328f80a826383cc1e7fb5bc09e9bc28b9b81c3d4
cweng1 <chengze-weng@uiowa.edu>
2019-10-08 13:34:42 -0500

    modified application to use basic Jinja templates

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world!'
+    return render_template('index.html', pageTitle='Flask Server Home Page')
 
 @app.route('/chengze')
 def chengze():
-    return 'Hello Chengze!'
+    return render_template('chengze.html', pageTitle='About Chengze')
 
 if __name__ == '__main__':
     app.run(debug=True)
