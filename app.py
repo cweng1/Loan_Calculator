ff78d1065738561257f3dfb4fcb36abc06db7e33
cweng1 <chengze-weng@uiowa.edu>
2019-10-10 14:49:50 -0500

    modified programming

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', display="", pageTitle='Loan Calculator')
+    return render_template('index.html', display="Discount: ",display2="Loan Payment: ", pageTitle='Loan Calculator')
 
 
 @app.route('/loan', methods=['GET', 'POST'])
@@ -16,13 +16,13 @@ def addition():
           LoanAmount  = int(form['A'])
           Number_of_periodic_payments = int(form['N'])
           Periodic_interest_rate = float(form['I'])
-          Discount =   ((( 1 + Periodic_interest_rate ) **Number_of_periodic_payments ) - 1 ) / ( Periodic_interest_rate* ( 1+ Periodic_interest_rate) **Number_of_periodic_payments)
-          Loan_Payment = LoanAmount/Discount
-          return render_template('index.html', display=(('Discount:',Discount),('Loan Payment:',Loan_Payment)), pageTitle='My Calculator')
+          Discount =   round(((( 1 + Periodic_interest_rate ) **Number_of_periodic_payments ) - 1 ) / ( Periodic_interest_rate* ( 1+ Periodic_interest_rate) **Number_of_periodic_payments),2)
+          Loan_Payment = round(LoanAmount/Discount,2)
+          return render_template('index.html', display=(f'Discount: {Discount}'),display2=(f'Loan Payment: ${Loan_Payment}'), pageTitle='Loan Calculator')
 
 
       except ValueError:
-          return render_template('index.html', display="Please enter two integers.", pageTitle='Loan Calculator')
+          return render_template('index.html', display="Please enter numbers.", pageTitle='Loan Calculator')
 
     return redirect("/")
 
